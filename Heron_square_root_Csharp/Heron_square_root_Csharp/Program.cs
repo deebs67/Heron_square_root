69e9e683671fea3071a8c2bcc3bcf98351c572e5
ddnb <damian.bevan@aceaxis.co.uk>
2020-02-11 13:50:16 +0000

    First checkin of C# (placeholder) code, along with various Visual Studio solution files etc.

---
+++ Heron_square_root_Csharp/Heron_square_root_Csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heron_square_root_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Heron's square-root";
+            Console.WriteLine("Heron's square-root computation method (placeholder). Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
