e38c9912ce6af546927ada60a4c56545326bb4ab
deebs67 <deebs67@yahoo.co.uk>
2020-02-12 00:35:19 +0000

    First complete implementation, plus some additions to README.md

--- Heron_square_root_Csharp/Heron_square_root_Csharp/Program.cs
+++ Heron_square_root_Csharp/Heron_square_root_Csharp/Program.cs
@@ -10,9 +10,37 @@ namespace Heron_square_root_Csharp
     {
         static void Main(string[] args)
         {
-            Console.Title = "Heron's square-root";
-            Console.WriteLine("Heron's square-root computation method (placeholder). Press any key to continue...");
+            Console.Title = "Heron's square-root computation method";
+
+            // Declare variables
+            double area_of_square = 10.0;
+            int number_of_iterations = 10;
+            
+            Console.WriteLine("\nHeron's square-root computation method");
+            Console.WriteLine("======================================\n");
+
+            // Make first guess the area of the square
+            double horizontal_side = area_of_square;
+
+            // Now iterate towards better guesses
+            for (int this_iteration = 0; this_iteration < number_of_iterations; this_iteration++)
+            {
+                Console.WriteLine($"This iteration: {this_iteration},  square-root-guess: {horizontal_side}"); // Value going into this iteration
+                horizontal_side = update_horizontal_side(area_of_square, horizontal_side);  // Iterative update
+            }
+
+            // Write out the final result, and exit.
+            Console.WriteLine($"\nThe calculated square root of {area_of_square} from {number_of_iterations} iterations is {horizontal_side}");
+            Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
+        }
+        static double update_horizontal_side(double area_of_square, double horizontal_side)
+        // Carries out one iteration of Heron's algorithm, to improve the guess as to the square root
+        // It does this by defining a rectangle with the given area. Since one side of this rectangle will
+        // be too short, and the other too long, a better next guess is the average of the two.
+        {
+            double vertical_side = area_of_square / horizontal_side;
+            return (horizontal_side + vertical_side) / 2.0;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Heron_square_root
-Implementation(s) of the iterative square-root calculation method due to Heron ('Hero') of Alexandria
+Implementation(s) of the iterative square-root calculation method due to Heron ('Hero') of Alexandria.
+
+We have implemented the algorithm as a Console application in the Csharp language, with default
+values of an area of 10.0 and 10 iterations. Hand-hack the source file 'Program.cs' to change these
+values.
+
